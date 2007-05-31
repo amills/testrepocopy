a52127f95642b85fee21b27539c5568a617c8f1b
kowalski <kowalski@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-05-31 15:37:39 +0000

    change to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,9 +5,9 @@ set :repository, "file:///home/svn/repos/#{application}/trunk"
 set :user, "cappy"
 set :svn, "/opt/csw/bin/svn"
 
-role :web, "dev1.ublip.com", "dev2.ublip.com", "dev3.ublip.com"
-role :app, "dev1.ublip.com", "dev2.ublip.com", "dev3.ublip.com"
-role :db, "dev1.ublip.com", "dev2.ublip.com", "dev3.ublip.com", :primary => true
+role :web, "dev2.ublip.com"
+role :app, "dev2.ublip.com"
+role :db, "dev2.ublip.com", :primary => true
 
 set :deploy_to, "/opt/www/dev/#{application}" 
 
