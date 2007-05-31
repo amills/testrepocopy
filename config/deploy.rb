909a49f19de0643619c6794dffeb88f9501957e1
kowalski <kowalski@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-05-31 18:24:53 +0000

    deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,13 @@
 require 'mongrel_cluster/recipes'
 
+set :use_sudo, false
+
 set :application, "Ublip"
 set :repository, "file:///home/svn/repos/#{application}/trunk"
 set :user, "cappy"
 set :svn, "/opt/csw/bin/svn"
 set :sudo, "/opt/csw/bin/sudo"
-set :"mongrel_rails", "/opt/csw/bin/mongrel_rails"
+
 
 role :web, "dev2.ublip.com"
 role :app, "dev2.ublip.com"
