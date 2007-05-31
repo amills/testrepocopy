035a8beb8c5a78dcc2e4e96dd0774544bac44c00
kowalski <kowalski@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-05-31 15:31:00 +0000

    change to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :user, "cappy"
 
 role :web, "dev1.ublip.com", "dev2.ublip.com", "dev3.ublip.com"
 role :app, "dev1.ublip.com", "dev2.ublip.com", "dev3.ublip.com"
+role :db, "dev1.ublip.com", "dev2.ublip.com", "dev3.ublip.com", :primary => true
 
 set :deploy_to, "/opt/www/dev/#{application}" 
 
