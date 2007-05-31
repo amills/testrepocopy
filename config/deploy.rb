d226daaa894580decd8ac7f41f2571af10614dcf
kowalski <kowalski@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-05-31 15:35:24 +0000

    change to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ require 'mongrel_cluster/recipes'
 set :application, "Ublip"
 set :repository, "file:///home/svn/repos/#{application}/trunk"
 set :user, "cappy"
+set :svn, "/opt/csw/bin/svn"
 
 role :web, "dev1.ublip.com", "dev2.ublip.com", "dev3.ublip.com"
 role :app, "dev1.ublip.com", "dev2.ublip.com", "dev3.ublip.com"
