411148eee7c46cbabbac68462b21fd7509002f25
kowalski <kowalski@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-05-31 15:29:01 +0000

    added user to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ require 'mongrel_cluster/recipes'
 
 set :application, "ublip"
 set :repository, "file:////svn.ublip.com/home/svn/repos/#{application}/trunk"
-
+set :user, "cappy"
 
 role :web, "dev1.ublip.com", "dev2.ublip.com", "dev3.ublip.com"
 role :app, "dev1.ublip.com", "dev2.ublip.com", "dev3.ublip.com"
