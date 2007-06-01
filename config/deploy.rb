66c7dfd8829270ea5d77a330caf3d7c6b399858f
kowalski <kowalski@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-06-01 23:17:59 +0000

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repository, "svn+ssh://svn.ublip.com/home/svn/repos/#{application}/trunk"
 set :user, "cappy"
 set :svn, "/opt/csw/bin/svn"
 set :sudo, "/opt/csw/bin/sudo"
-
+ssh_options[:paranoid] = false
 
 role :web, "dev2.ublip.com"
 role :app, "dev2.ublip.com"
