c2b2434f39f31889accfa21b9e1200a9f03f98b3
kowalski <kowalski@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2007-06-05 15:39:07 +0000

    new cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,21 @@
 require 'mongrel_cluster/recipes'
 
 
-set :application, "Ublip"
-#set :repository, "file:///home/svn/repos/#{application}/trunk"
-set :repository, "svn+ssh://svn.ublip.com/home/svn/repos/#{application}/trunk"
+set :svnstring, "Ublip"
+set :application, "dev2.ublip.com"
+
+
+set :repository, "svn+ssh://svn.ublip.com/home/svn/repos/#{svnstring}/trunk"
 set :user, "cappy"
 set :svn, "/opt/csw/bin/svn"
 set :sudo, "/opt/csw/bin/sudo"
 ssh_options[:paranoid] = false
 
-role :web, "dev2.ublip.com"
-role :app, "dev2.ublip.com"
-role :db, "dev2.ublip.com"
+role :web, application
+role :app, application
+role :db, application, :primary => true
+role :db, application
 
-set :deploy_to, "/opt/www/dev/#{application}" 
+set :deploy_to, "/opt/www/dev/apps/#{application}" 
 
 set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
