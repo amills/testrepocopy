b96fa537b0454c0ec8f42ff1af8c448b12526fa0
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-03-28 17:13:53 +0000

    Config changes.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,12 @@
 require 'mongrel_cluster/recipes'
 
 set :application, "rapid2.ublip.com"
-set :repository,  "https://ublip.svn.ey01.engineyard.com/Ublip_rapid/branches/rapid2"
+set :repository,  "https://ublip.svn.ey01.engineyard.com/Ublip_v2/branches/rapid2"
 set :scm_username,  "deploy"
 set :scm_password,  "wucr5ch8v0"
-set :user,        "gmns9faa"
+set :user,        "admin"
 set :password,    "gggdwustzx"
-set :deploy_to,     "/opt/ublip/rapid"
+set :deploy_to,     "/opt/ublip/rails"
 set :rails_env, "production"
 set :svn, "/opt/csw/bin/svn"
 set :sudo, "/opt/csw/bin/sudo"
