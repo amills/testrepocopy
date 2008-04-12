e252602028807284c203b1c3ca2df8e70e052987
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-04-12 06:30:10 +0000

    Sanav gateway.

--- app/controllers/ws_controller.rb
+++ app/controllers/ws_controller.rb
@@ -35,4 +35,51 @@ class WsController < ApplicationController
        render_text "We're sorry, this device does not exist"
     end
   end
+  
+  # Simple Sanav integration
+  # Request from logs Parameters: {"action"=>"sanav", "controller"=>"ws", "imei"=>"352023003616634", "rmc"=>"$GPRMC,055104.000,A,2906.5967,N,08058.9034,W,0.00,0.00,120408,,*10,AUTO"}
+  def sanav
+    device = Device.find_by_imei(params[:imei])
+    
+    if device
+      rmc = params[:rmc]
+      
+      # Convert lat lng from degrees minutes to decimal degrees
+      rmc = rmc.split(",")
+      lat = rmc[3]
+      lng = rmc[5]
+
+      lat_deg = lat.slice(0,2)
+      lng_deg = lng.slice(1,2)
+
+      lat_min = lat.slice(2, lat.length-2)
+      lng_min = lng.slice(2, lng.length-2)
+
+      lat_min = lat_min.to_f/60
+      lng_min = lng_min.to_f/60
+
+      lat = lat_deg.to_f+lat_min.to_f
+      lng = lng_deg.to_f+lng_min.to_f
+      
+      reading = Reading.new
+      reading.latitude = lat
+      reading.longitude = lng
+      
+      # Save the reading
+      if reading.save
+        # Save the reading id with the device
+        device.recent_reading_id = reading.id
+        device.save
+        render_text "Success"
+      else
+        render_text "Error saving data"
+      end
+    # Error finding device
+      
+    else
+      render_text "We're sorry, this device does not exist"
+    end
+      
+    end
+  end
 end
