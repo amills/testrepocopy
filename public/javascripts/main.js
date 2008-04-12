fac5cff2fea51e87a95068d3d231238e0de0ab90
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-04-12 05:53:08 +0000

    Adding horizontal accuracy for iPhone test.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -73,6 +73,7 @@ function getRecentReadings(redrawMap,id) {
 		var dts = xml.documentElement.getElementsByTagName("dt");
 		var addresses = xml.documentElement.getElementsByTagName("address");
 		var notes = xml.documentElement.getElementsByTagName("note");
+		var haccuracy = xml.documentElement.getElementsByTagName("haccuracy");
 		var icon_id = xml.documentElement.getElementsByTagName("icon_id");
 		
 		for(var i = 0; i < lats.length; i++) {
@@ -113,14 +114,15 @@ function getRecentReadings(redrawMap,id) {
 				if(notes[i].firstChild != undefined)
 					note = notes[i].firstChild.nodeValue;
 					
-				var device = {id: ids[i].firstChild.nodeValue, name: names[i].firstChild.nodeValue, lat: lats[i].firstChild.nodeValue, lng: lngs[i].firstChild.nodeValue, address: address, dt: dts[i].firstChild.nodeValue, note: note};
+				var device = {id: ids[i].firstChild.nodeValue, name: names[i].firstChild.nodeValue, lat: lats[i].firstChild.nodeValue, lng: lngs[i].firstChild.nodeValue, address: address, dt: dts[i].firstChild.nodeValue, note: note, haccuracy: haccuracy[i].firstChild.nodeValue};
 				devices.push(device);
 				
 				// Populate the table
 				var row = $("row" + device.id);
 				var tds = row.getElementsByTagName("td");
 				tds[2].innerHTML = device.address;
-				tds[3].innerHTML = device.dt;
+				tds[3].innerHTML = device.haccuracy;
+				tds[4].innerHTML = device.dt;
 				
 		        var point = new GLatLng(device.lat, device.lng);
 				gmap.addOverlay(createMarker(device.id, point, iconALL, createDeviceHtml(device.id)));
