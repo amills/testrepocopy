e5bb4717a84923d609e076ceeedfd24bb50d0c30
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-04-28 05:27:39 +0000

    Config tweaks to switch between digi and fleet app.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,8 @@ set :deploy_to,     "/opt/ublip/rails"
 set :rails_env, "production"
 set :svn, "/opt/csw/bin/svn"
 set :sudo, "/opt/csw/bin/sudo"
+before "deploy:update_code", "determine_env"
 after "deploy:update_code", "symlink_configs"
-
 ssh_options[:paranoid] = false
 
 role :web, application
@@ -21,10 +21,22 @@ role :db, application
 
 set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 
+namespace :deploy do
+  task :restart, :roles => :app do
+    run "cd /opt/ublip/rails/current; mongrel_rails cluster::restart;"
+  end
+end
+
 task :symlink_configs, :roles => :app, :except => {:no_symlink => true} do
    run <<-CMD
      cd #{release_path} &&
      ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml &&
      ln -nfs #{shared_path}/config/mongrel_cluster.yml #{release_path}/config/mongrel_cluster.yml
    CMD
- end
+end
+
+task :determine_env do
+  if app == "ublip"
+    set :repository,  "https://ublip.svn.ey01.engineyard.com/Ublip_v2/trunk"
+  end
+end
