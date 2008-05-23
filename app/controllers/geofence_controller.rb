3b03d9fb278fea87e82fd2b985938dc8866eb094
kbakal <kbakal@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-05-23 14:43:16 +0000

    1. css design changes in geofence.css 2. Address validations. 3. cover security issues.

--- app/controllers/geofence_controller.rb
+++ app/controllers/geofence_controller.rb
@@ -2,18 +2,22 @@ class GeofenceController < ApplicationController
   before_filter :authorize
 
   def index
-    device_ids = Device.get_devices(session[:account_id]).map{|x| x.id}
-    @geofences_pages,@geofences = paginate :geofences,:conditions => ["device_id in (#{device_ids.join(',')}) or account_id = ?",session[:account_id]], :order => "name",:per_page => 3
+    device_ids = Device.get_devices(session[:account_id]).map{|x| x.id}        
+    if device_ids.empty?       
+      @geofences_pages,@geofences = paginate :geofences,:conditions => ["account_id = ?",session[:account_id]], :order => "name",:per_page => 3  
+    else
+      @geofences_pages,@geofences = paginate :geofences,:conditions => ["device_id in (#{device_ids.join(',')}) or account_id = ?",session[:account_id]], :order => "name",:per_page => 3  
+    end    
   end
   
   def new 
-    @devices = Device.get_devices(session[:account_id])
+    @devices = Device.get_devices(session[:account_id])    
     if request.post?
       geofence = Geofence.new
       add_and_edit(geofence)
       if geofence.save
         flash[:message] = 'Geofence created succesfully'
-        redirect_to :controller => 'geofence', :action => 'index'
+        redirect_to geofence_url
       else
         flash[:message] = 'Geofence not created'
       end
@@ -26,17 +30,21 @@ class GeofenceController < ApplicationController
   
   def edit 
     @devices = Device.get_devices(session[:account_id])
-    @geofence = Geofence.find(params[:id])
-    if request.post?
-      geofence = Geofence.find(params[:id])
-      add_and_edit(geofence)
-      if geofence.save
-        flash[:message] = 'Geofence updated succesfully'
-        redirect_to :controller => 'geofence', :action => 'index'
-      else
-        flash[:message] = 'Geofence not updated'
-      end
-    end  
+    @geofence = Geofence.find(params[:id])     
+    if check_action_for_user
+      if request.post?           
+       add_and_edit(@geofence)
+       if @geofence.save
+          flash[:message] = 'Geofence updated succesfully'
+         redirect_to geofence_url
+       else
+         flash[:message] = 'Geofence not updated'
+       end
+      end  
+     else
+         redirect_to geofence_url
+        flash[:message] = 'Invalid action.'
+     end    
   end  
   
   def view  
@@ -71,20 +79,22 @@ class GeofenceController < ApplicationController
   end  
   
   def delete 
-    Geofence.delete(params[:id])
-    flash[:message] = 'Geofence deleted successfully'
-    redirect_to :action => "index"
+     @geofence=Geofence.find_by_id(params[:id]) 
+     if check_action_for_user 
+        Geofence.delete(params[:id])
+        flash[:message] = 'Geofence deleted successfully'
+     else
+        flash[:message] = 'Invalid action.'   
+     end    
+     redirect_to geofence_url
   end  
 
 private
   
   def add_and_edit(geofence) 
-    add = params[:address].split(',')
-    lat = add[0]
-    lng = add[1]
+    add=params[:address].split(',')
+    geofence.latitude , geofence.longitude = add[0] , add[1]
     geofence.name = params[:name]
-    geofence.latitude= lat
-    geofence.longitude = lng
     geofence.radius = params[:radius]
     geofence.address = params[:address]
     geofence.account_id = params[:radio] == "1" ? session[:account_id] : 0
