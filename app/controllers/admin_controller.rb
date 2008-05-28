f8db9078bbf5646435eea64a33290976b40f020b
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-05-28 21:45:56 +0000

    Small change for merge test.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,6 +5,5 @@ class AdminController < ApplicationController
     @total_accounts = Account.count(:conditions => "is_deleted = 0")
     @total_users = User.count 
     @total_devices = Device.count(:conditions => "provision_status_id = 1")
-  end
-  
+  end  
 end
