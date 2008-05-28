03560c279a95d3410a07332726178c8b6dddd732
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-05-28 05:41:31 +0000

    Sanav fix.

--- app/controllers/ws_controller.rb
+++ app/controllers/ws_controller.rb
@@ -36,50 +36,52 @@ class WsController < ApplicationController
     end
   end
   
+  # Simple Sanav integration
+  # Request from logs Parameters: {"action"=>"sanav", "controller"=>"ws", "imei"=>"352023003616634", "rmc"=>"$GPRMC,055104.000,A,2906.5967,N,08058.9034,W,0.00,0.00,120408,,*10,AUTO"}
   def sanav
-      device = Device.find_by_imei(params[:imei])
+    device = Device.find_by_imei(params[:imei])
+    
+    if device
+      rmc = params[:rmc]
+      
+      # Convert lat lng from degrees minutes to decimal degrees
+      rmc = rmc.split(",")
+      lat = rmc[3].to_f.to_s
+      lng = rmc[5].to_f.to_s
+      spd = rmc[7]
+      
+      lat_deg = lat.slice(0,2).to_f
+      lng_deg = lng.slice(0,3).to_f
 
-      if device
-        rmc = params[:rmc]
+      lat_min = lat.slice(2, lat.length-2)
+      lng_min = lng.slice(3, lng.length-2)
 
-        # Convert lat lng from degrees minutes to decimal degrees
-        rmc = rmc.split(",")
-        lat = rmc[3].to_f.to_s
-        lng = rmc[5].to_f.to_s
-        spd = rmc[7]
+      lat_min = lat_min.to_f/60
+      lng_min = lng_min.to_f/60
 
-        lat_deg = lat.slice(0,2)
-        lng_deg = lng.slice(0,2)
-
-        lat_min = lat.slice(2, lat.length-2)
-        lng_min = lng.slice(2, lng.length-2)
-
-        lat_min = lat_min.to_f/60
-        lng_min = lng_min.to_f/60
-
-        lat = lat_deg.to_f+lat_min.to_f
-        lng = lng_deg.to_f+lng_min.to_f
-
-        reading = Reading.new
-        reading.latitude = lat
-        reading.longitude = "-" + lng.to_s
-        reading.device_id = device.id
-        reading.speed = spd
-        reading.event_type = "DEFAULT"
-
-        # Save the reading
-        if reading.save
-          # Save the reading id with the device
-          device.recent_reading_id = reading.id
-          device.save
-          render_text "Success"
-        else
-          render_text "Error saving data"
-        end
-      # Error finding device
-
+      lat = lat_deg.to_f+lat_min.to_f
+      lng = lng_deg.to_f+lng_min.to_f
+      
+      reading = Reading.new
+      reading.latitude = lat
+      reading.longitude = "-" + lng.to_s
+      reading.device_id = device.id
+      reading.speed = spd
+      reading.event_type = "DEFAULT"
+      
+      # Save the reading
+      if reading.save
+        # Save the reading id with the device
+        device.recent_reading_id = reading.id
+        device.save
+        render_text "Success"
       else
-        render_text "We're sorry, this device does not exist"
+        render_text "Error saving data"
       end
+    # Error finding device
+      
+    else
+      render_text "We're sorry, this device does not exist"
     end
+  end
 end
