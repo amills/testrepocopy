2dfcbfcdbe0250c1dedd7b270d5ab5eb3cd3487f
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-05-31 02:10:56 +0000

    Fix for sending mail on SliceHost.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,3 +17,8 @@ config.action_controller.perform_caching             = true
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 ActionMailer::Base.delivery_method = :sendmail
+
+ActionMailer::Base.sendmail_settings = {
+  :location => "/opt/csw/sbin/sendmail",
+  :arguments => "-t"
+}
