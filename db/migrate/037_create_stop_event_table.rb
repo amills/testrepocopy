1cb3d3561936e217254fc4845280627f3367676e
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-06-07 21:04:53 +0000

    Reverting to float for testing

--- db/migrate/037_create_stop_event_table.rb
+++ db/migrate/037_create_stop_event_table.rb
@@ -1,8 +1,8 @@
 class CreateStopEventTable < ActiveRecord::Migration
   def self.up
     create_table :stop_events  do |t|
-     t.column "latitude",     :decimal, :precision => 15, :scale => 10
-     t.column "longitude",    :decimal, :precision => 15, :scale => 10
+     t.column "latitude",     :float
+     t.column "longitude",    :float
      t.column "duration",     :integer
      t.column "device_id",    :integer
      t.column "created_at",  :datetime
