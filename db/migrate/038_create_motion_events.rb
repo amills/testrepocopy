20ed520831ce1f87944a08f32c096915c154a20d
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-06-07 20:38:07 +0000

    Changed float to decimal since float truncates

--- db/migrate/038_create_motion_events.rb
+++ db/migrate/038_create_motion_events.rb
@@ -1,8 +1,8 @@
 class CreateMotionEvents < ActiveRecord::Migration
   def self.up
     create_table :motion_events do |t|
-      t.column "latitude",     :float
-      t.column "longitude",    :float
+      t.column "latitude",     :decimal, :precision => 15, :scale => 10
+      t.column "longitude",    :decimal, :precision => 15, :scale => 10
       t.column "notified",     :boolean, :default => false
       t.column "device_id",    :integer
       t.column "created_at",  :datetime
