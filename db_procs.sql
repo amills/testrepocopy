acee83baf3de3732ef82b613435bc8d4e7888baf
sdate <sdate@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-07-10 04:04:43 +0000

    changes made by Dennis

--- db_procs.sql
+++ db_procs.sql
@@ -20,7 +20,8 @@ CREATE PROCEDURE insert_stop_event(
 	_latitude FLOAT,
 	_longitude FLOAT,
 	_modem VARCHAR(22),
-	_created DATETIME
+	_created DATETIME,
+	_reading_id INT(11)
 )
 BEGIN
 	DECLARE deviceID INT(11);
@@ -31,8 +32,8 @@ BEGIN
 	IF deviceID IS NOT NULL THEN
 		SELECT id INTO latestStopID FROM stop_events WHERE device_id=deviceID AND created_at <= _created ORDER BY created_at desc limit 1;
 		IF (SELECT id FROM stop_events WHERE id=latestStopID AND duration IS NULL and distance(_latitude, _longitude, latitude, longitude) < 0.1) IS NULL THEN
-			INSERT INTO stop_events (latitude, longitude, created_at, device_id)
-		   		VALUES (_latitude, _longitude, _created, deviceID);
+			INSERT INTO stop_events (latitude, longitude, created_at, device_id, reading_id)
+		   		VALUES (_latitude, _longitude, _created, deviceID, _reading_id);
 		END IF;
 	END IF;
 END;;
@@ -60,7 +61,7 @@ BEGIN
 		  
 		IF first_move_after_stop_id IS NOT NULL THEN
 			SELECT TIMESTAMPDIFF(MINUTE, stopTime, created_at) INTO stopDuration FROM readings where id=first_move_after_stop_id;
-			UPDATE stop_events SET duration = stopDuration where id=eventID;
+			UPDATE stop_events SET duration = stopDuration+3 where id=eventID;
 		END IF;
 		
 		SELECT COUNT(*) INTO num_events_to_check FROM open_stop_events WHERE checked=FALSE;
