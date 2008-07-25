1c021215e82f4532b4d242206e94c11787cdbc0c
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-07-25 18:20:22 +0000

    Multi sensor demo config for Agrekko

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require 'mongrel_cluster/recipes'
 
 set :application, "digi.ublip.com"
-set :repository,  "https://ublip.svn.ey01.engineyard.com/Ublip_v2/branches/digi"
+set :repository,  "https://ublip.svn.ey01.engineyard.com/Ublip_v2/branches/digi_multi_sensor"
 set :scm_username,  "deploy"
 set :scm_password,  "wucr5ch8v0"
 set :user,        "admin"
@@ -10,7 +10,7 @@ set :deploy_to,     "/opt/ublip/rails"
 set :rails_env, "production"
 set :svn, "/opt/csw/bin/svn"
 set :sudo, "/opt/csw/bin/sudo"
-before "deploy:update_code", "determine_env"
+#before "deploy:update_code", "determine_env"
 after "deploy:update_code", "symlink_configs"
 ssh_options[:paranoid] = false
 
@@ -34,9 +34,10 @@ task :symlink_configs, :roles => :app, :except => {:no_symlink => true} do
      ln -nfs #{shared_path}/config/mongrel_cluster.yml #{release_path}/config/mongrel_cluster.yml
    CMD
 end
-
+=begin
 task :determine_env do
   if app == "ublip"
     set :repository,  "https://ublip.svn.ey01.engineyard.com/Ublip_v2/trunk"
   end
 end
+=end
