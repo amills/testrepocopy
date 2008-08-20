e93946ffecbac819e7b16b63c7b7ffd0e81a52cd
kbakal <kbakal@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-08-20 19:05:30 +0000

    expand button working on reports pages

--- public/javascripts/scrolling_effect.js
+++ public/javascripts/scrolling_effect.js
@@ -1,35 +1,26 @@
 // scrolling effect start
-var ieHoffset_extra=document.all? 15 : 0
-
+ var ieHoffset_extra=document.all? 15 : 0
  var dsocleft=document.all? iecompattest().scrollLeft : pageXOffset
  var dsoctop=document.all? iecompattest().scrollTop : pageYOffset
-  var bName = navigator.appName;
-  
+ var bName = navigator.appName;  
   if (bName == "Microsoft Internet Explorer" ){       
-    var Hoffset = (document.documentElement.clientWidth - ieHoffset_extra) - (document.documentElement.clientWidth - ieHoffset_extra)/2 +10
-    var Voffset = document.documentElement.clientHeight - 160 
+    var Hoffset =  (document.documentElement.clientWidth - ieHoffset_extra) - (document.documentElement.clientWidth - ieHoffset_extra)/2 +10
+    var Voffset =  document.documentElement.clientHeight - 160 
     distance_top = 160
   }
   else{  
     var Hoffset = ieNOTopera? iecompattest().clientWidth+ieHoffset_extra : window.innerWidth+ieHoffset_extra -  (document.documentElement.clientWidth - ieHoffset_extra)/2 - 10
-    var Voffset= ieNOTopera? iecompattest().clientHeight : window.innerHeight - 155
-    distance_top = 160
+    var Voffset=  ieNOTopera? iecompattest().clientHeight : window.innerHeight - 155
+    distance_top = 300
  }
- 
- 
-
-var thespeed=3 
-
-var ieNOTopera=document.all&&navigator.userAgent.indexOf("Opera")==-1
-var myspeed=0
-
-
-var cross_obj=document.all? document.all.map : document.getElementById? document.getElementById("map") : document.map
+ var thespeed=3 
+ var ieNOTopera=document.all&&navigator.userAgent.indexOf("Opera")==-1
+ var myspeed=0
+ var cross_obj=document.all? document.all.map : document.getElementById? document.getElementById("map") : document.map
 
 function iecompattest(){
     return (document.compatMode && document.compatMode!="BackCompat")? document.documentElement : document.body
 }
-
 
 function positionit(){
     var dsocleft=document.all? iecompattest().scrollLeft : pageXOffset
@@ -38,13 +29,13 @@ function positionit(){
     var window_height=ieNOTopera? iecompattest().clientHeight : window.innerHeight 
     
     if (document.all||document.getElementById){ 
-        cross_obj.style.left=parseInt(dsocleft)+parseInt(window_width)-Hoffset +"px"
+        cross_obj.style.left= fullScreenMap? 30 : parseInt(dsocleft)+parseInt(window_width)-Hoffset +"px"
         var scrolltop = 0;
         if(document.documentElement.scrollTop >= 0 && document.documentElement.scrollTop <= distance_top)
           scrolltop =  document.documentElement.scrollTop;
         else
          scrolltop = distance_top;
-        cross_obj.style.top=(dsoctop+parseInt(window_height)-Voffset -  scrolltop)+"px"
+        cross_obj.style.top= fullScreenMap? 150 : (dsoctop+parseInt(window_height)-Voffset -  scrolltop)+"px"
     }
     else if (document.layers){
          cross_obj.left=dsocleft+window_width-Hoffset
