a81a7707387ef71a9c524fc78a00a512b2d2e28f
kbakal <kbakal@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-08-26 12:30:15 +0000

    modified the javascript to stay aligned even when the window is reduced

--- public/javascripts/scrolling_effect.js
+++ public/javascripts/scrolling_effect.js
@@ -6,44 +6,46 @@
  var distance_top;
  var temp_value1;
  var temp_value2;
+ var myspeed = 0,cross_obj,arrow_obj,bName;
+ var Hoffset,Voffset,HoffsetArrow,VoffsetArrow
+ 
+ function initialiaze(){
  from_main? temp_value1 = 128 : temp_value1 = 160     
- from_main? temp_value2 = 115 : temp_value2 = 155     
+ from_main? temp_value2 = 112 : temp_value2 = 155     
  
+ if (from_main)
+     cross_obj=document.all? document.all.header_ddd : document.getElementById? document.getElementById("header_ddd") : document.header_ddd
+ else
+     cross_obj=document.all? document.all.map : document.getElementById? document.getElementById("map") : document.map
  
- var bName = navigator.appName;  
+   arrow_obj = document.all? document.all.arrow_expand : document.getElementById? document.getElementById("arrow_expand") : document.arrow_expand
+
+  bName = navigator.appName;  
   if (bName == "Microsoft Internet Explorer" ){       
-    var Hoffset =  (document.documentElement.clientWidth - ieHoffset_extra) - (document.documentElement.clientWidth - ieHoffset_extra)/2  + addtional_value
-    var Voffset =  document.documentElement.clientHeight - temp_value1    
-    var  HoffsetArrow =  (document.documentElement.clientWidth - ieHoffset_extra) - (document.documentElement.clientWidth - ieHoffset_extra)/2 + 20
-    var  VoffsetArrow = document.documentElement.clientHeight - temp_value1*3      
-    from_main? distance_top = 115 : distance_top = 160
+     Hoffset =  (document.documentElement.clientWidth - ieHoffset_extra) - (document.documentElement.clientWidth - ieHoffset_extra)/2  + addtional_value
+     Voffset =  document.documentElement.clientHeight - temp_value1    
+     HoffsetArrow =  (document.documentElement.clientWidth - ieHoffset_extra) - (document.documentElement.clientWidth - ieHoffset_extra)/2 + 20
+     VoffsetArrow = document.documentElement.clientHeight - temp_value1*3      
+     from_main? distance_top = 112 : distance_top = 160
   }
   else{  
-    var Hoffset = ieNOTopera? iecompattest().clientWidth+ieHoffset_extra : window.innerWidth+ieHoffset_extra -  (document.documentElement.clientWidth - ieHoffset_extra)/2 - addtional_value
-    var Voffset=  ieNOTopera? iecompattest().clientHeight : window.innerHeight - temp_value2
-    var  HoffsetArrow = ieNOTopera? iecompattest().clientWidth+ieHoffset_extra : window.innerWidth+ieHoffset_extra -  (document.documentElement.clientWidth - ieHoffset_extra)/2
-    var  VoffsetArrow = ieNOTopera? iecompattest().clientHeight : window.innerHeight - temp_value2*3
-     from_main? distance_top = 115 :  distance_top = 150
+     Hoffset = ieNOTopera? iecompattest().clientWidth+ieHoffset_extra : window.innerWidth+ieHoffset_extra -  (document.documentElement.clientWidth - ieHoffset_extra)/2 - addtional_value
+     Voffset=  ieNOTopera? iecompattest().clientHeight : window.innerHeight - temp_value2
+     HoffsetArrow = ieNOTopera? iecompattest().clientWidth+ieHoffset_extra : window.innerWidth+ieHoffset_extra -  (document.documentElement.clientWidth - ieHoffset_extra)/2
+     VoffsetArrow = ieNOTopera? iecompattest().clientHeight : window.innerHeight - temp_value2*3
+     from_main? distance_top = 112 :  distance_top = 150
  }
+}
  var thespeed=3 
  var ieNOTopera=document.all&&navigator.userAgent.indexOf("Opera")==-1
- var myspeed=0
- if (from_main)
-    var cross_obj=document.all? document.all.header_ddd : document.getElementById? document.getElementById("header_ddd") : document.header_ddd
- else
-    var cross_obj=document.all? document.all.map : document.getElementById? document.getElementById("map") : document.map
- 
-  var arrow_obj = document.all? document.all.arrow_expand : document.getElementById? document.getElementById("arrow_expand") : document.arrow_expand
  
 function iecompattest(){
     return (document.compatMode && document.compatMode!="BackCompat")? document.documentElement : document.body
 }
 
 function positionit(){
+	initialiaze();
     var dsocleft=document.all? iecompattest().scrollLeft : pageXOffset
-    var Voffset=  ieNOTopera? iecompattest().clientHeight : window.innerHeight - temp_value2
-    var Hoffset = ieNOTopera? iecompattest().clientWidth+ieHoffset_extra : window.innerWidth+ieHoffset_extra -  (document.documentElement.clientWidth - ieHoffset_extra)/2 - addtional_value
-
     var dsoctop=document.all? iecompattest().scrollTop : pageYOffset
     var window_width=ieNOTopera? iecompattest().clientWidth+ieHoffset_extra : window.innerWidth+ieHoffset_extra
     var window_height=ieNOTopera? iecompattest().clientHeight : window.innerHeight 
@@ -53,6 +55,8 @@ function positionit(){
         arrow_obj.style.left = fullScreenMap? "1%" : parseInt(dsocleft)+parseInt(window_width)-HoffsetArrow +"px"    
        
 	 //cross_obj.style.left = '51%';
+	 //arrow_obj.style.left = '50%';
+	 //cross_obj.style.position = '';
         var scrolltop = 0;
 	 
         if(document.documentElement.scrollTop >= 0 && document.documentElement.scrollTop <= distance_top)
