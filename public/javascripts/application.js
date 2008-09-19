9df081a31088ad4e2283fca4415c716a49469da7
kbakal <kbakal@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-09-19 14:44:28 +0000

    small change

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,39 +4,39 @@ function select_action(obj,from)
   var selected_text = document.getElementById("type1").options[Index].text;              
     if (selected_text == "New group")
     {
-         szNewURL = "http://"+document.location.hostname+":3000/devices/new_group"         
+         szNewURL = "http://"+document.location.hostname+"/devices/new_group"         
         window.location.href=szNewURL;            
     }
     else if (selected_text == "Edit groups")
     {
-         szNewURL = "http://"+document.location.hostname+":3000/devices/groups" 
+         szNewURL = "http://"+document.location.hostname+"/devices/groups" 
         window.location.href=szNewURL;                
     }
     else
     {
       if (from=='from_reports')
       {
-         szNewURL = "http://"+document.location.hostname+":3000/reports?type="+document.getElementById('type1').value
+         szNewURL = "http://"+document.location.hostname+"/reports?type="+document.getElementById('type1').value
         window.location.href=szNewURL;                            
       }
       else if (from=='from_devices')
       {         
          var val=document.getElementById('type1').value;         
          if (val=="")
-           szNewURL = "http://"+document.location.hostname+":3000/devices"
+           szNewURL = "http://"+document.location.hostname+"/devices"
          else
-           szNewURL = "http://"+document.location.hostname+":3000/devices?type="+document.getElementById('type1').value         
+           szNewURL = "http://"+document.location.hostname+"/devices?type="+document.getElementById('type1').value         
            window.location.href=szNewURL;                                
       }
      else if (from =='from_geofence')
      {
        var val=document.getElementById('type1').value;         
        if(val=="")
-         szNewURL = "http://"+document.location.hostname+":3000/geofence"
+         szNewURL = "http://"+document.location.hostname+"/geofence"
        else if(val.split(' ')[0] == "device")  
-         szNewURL = "http://"+document.location.hostname+":3000/geofence?type=device&id="+val.split(' ')[1]         
+         szNewURL = "http://"+document.location.hostname+"/geofence?type=device&id="+val.split(' ')[1]         
        else
-        szNewURL = "http://"+document.location.hostname+":3000/geofence?type="+document.getElementById('type1').value         
+        szNewURL = "http://"+document.location.hostname+"/geofence?type="+document.getElementById('type1').value         
        window.location.href=szNewURL;                                    
      }     
       else         
