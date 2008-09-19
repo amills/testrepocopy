8d5c517f93578e9ad0a3746bf203765b7375c698
kbakal <kbakal@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-09-19 14:52:15 +0000

    small change

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -95,7 +95,7 @@ class Notifier < ActionMailer::Base
 
     # Email body substitutions
     @body["name"] = "#{user.first_name} #{user.last_name}"
-    @body["login"] = "kumar.bakal@betterlabs.net"#user.email
+    @body["login"] = user.email
     @body["url"] = url
     @body["app_name"] = "Ublip"
   end
@@ -108,14 +108,14 @@ class Notifier < ActionMailer::Base
 
     # Email body substitutions
     @body["name"] = "#{user.first_name} #{user.last_name}"
-    @body["login"] = "kumar.bakal@betterlabs.net"#user.email
+    @body["login"] = user.email
     @body["password"] = password
     @body["url"] = url 
     @body["app_name"] = "Ublip"
   end
 
   def notify_reading(user, action, reading)
-    @recipients = "kumar.bakal@betterlabs.net"#user.email
+    @recipients = user.email
     @from = "alerts@ublip.com"
     @subject = reading.device.name + ' ' + action
     @body["action"] = action
@@ -136,7 +136,7 @@ class Notifier < ActionMailer::Base
   end
 
   def setup_email(user)
-    @recipients = "kumar.bakal@betterlabs.net"#"#{user.email}"
+    @recipients = "#{user.email}"
     @from       = "support@ublip.com"
     @sent_on    = Time.now
     @headers['Content-Type'] = "text/plain; charset=utf-16"
@@ -145,7 +145,7 @@ class Notifier < ActionMailer::Base
   # Send email to support from contact page
   def app_feedback(email, subdomain, feedback)
     @from = "support@ublip.com"
-    @recipients = "kumar.bakal@betterlabs.net"#"support@ublip.com"
+    @recipients = "support@ublip.com"
     @subject = "Feedback from #{subdomain}.ublip.com"
     @body["feedback"] = feedback
     @body["sender"] = email
@@ -154,7 +154,7 @@ class Notifier < ActionMailer::Base
   # Send a confirmation when an order is placed
   def order_confirmation(order_id, cust, order_details, email, password, subdomain)
     @from = "orders@ublip.com"
-    @recipients = "kumar.bakal@betterlabs.net"#email
+    @recipients = email
     @bcc = "orders@ublip.com"
     @subject = "Thank you for ordering from Ublip"
     @body["order_id"] = order_id
