0b995a9c94b506b4a6e7d02a9a1d7aaf0968b70f
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-09-25 17:00:26 +0000

    removing previous staging build

---
+++ tags/current_staging_build/test/unit/motion_events_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class MotionEventsTest < Test::Unit::TestCase
+  fixtures :motion_events
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
