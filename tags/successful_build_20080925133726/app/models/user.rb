3f916c6ffed1f05f7808c7622d72279be6aba577
amills <amills@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-09-25 18:39:21 +0000

    successful build

---
+++ tags/successful_build_20080925133726/app/models/user.rb
@@ -0,0 +1,82 @@
+require 'digest/sha1'
+class User < ActiveRecord::Base
+  belongs_to :account
+  attr_accessor :password
+  validates_presence_of :first_name, :last_name, :email
+  validates_uniqueness_of :email, :scope => :account_id # No dupes within account
+  validates_confirmation_of :password
+  validates_length_of :password, :within => 6..30, :if => :password_required?
+  before_save :encrypt_password
+
+  # Authenticates a user by subdomain, email and unencrypted password.  Returns the user or nil.
+  #def self.authenticate(email, password)
+  def self.authenticate(subdomain, email, password)
+    
+    account = Account.find_by_subdomain(subdomain)
+    user = find_by_email_and_account_id(email, account.id )
+    
+    
+    
+    if (user && user.authenticated?(password) && user.account.is_verified)
+      user.update_attribute(:last_login_dt, Time.now)
+      user
+    else
+      nil # User does not belong
+    end
+  end
+
+  # Encrypts some data with the salt.
+  def self.encrypt(password, salt)
+    Digest::SHA1.hexdigest("--#{salt}--#{password}--")
+  end
+
+  # Encrypts the password with the user salt
+  def encrypt(password)
+    self.class.encrypt(password, salt)
+  end
+
+  def authenticated?(password)
+    crypted_password == encrypt(password)
+  end
+  
+  def generate_security_token(hours = nil)
+     self.encrypt(self.created_at.to_i) 
+  end
+
+  def remember_token?
+    remember_token_expires_at && Time.now.utc < remember_token_expires_at 
+  end
+
+  # These create and unset the fields required for remembering users between browser closes
+  def remember_me
+    self.remember_token_expires_at = 1.day.from_now
+    self.remember_token = Digest::SHA1.hexdigest("#{salt}--#{self.email}--#{self.remember_token_expires_at}")
+    self.password = "" # This bypasses password encryption, thus leaving password intact
+    self.save_with_validation(false)
+  end
+  
+  def forget_me
+    self.remember_token_expires_at = nil
+    self.remember_token = nil
+    self.password = "" # This bypasses password encryption, thus leaving password intact
+    self.save_with_validation(false)
+  end
+
+  def change_password(pass, confirm)
+    self.password = pass
+    self.password_confirmation = confirm
+    @new_password = true
+  end
+  
+  protected
+    # before filter 
+    def encrypt_password
+      return if password.blank?
+      self.salt = Digest::SHA1.hexdigest("--#{Time.now.to_s}--#{email}--") if new_record?
+      self.crypted_password = encrypt(password)
+    end
+    
+    def password_required?
+      crypted_password.blank? || !password.blank?
+    end
+end
