17961b85b4500966d32ff02c504bd2e7fa29faad
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-09-25 18:37:26 +0000

    copied from K1tracking slice

--- trunk/config/environments/slicehost.rb
+++ trunk/config/environments/slicehost.rb
@@ -1,24 +1,26 @@
 # Settings specified here will take precedence over those in config/environment.rb
 
-# In the development environment your application's code is reloaded on
-# every request.  This slows down response time but is perfect for development
-# since you don't have to restart the webserver when you make code changes.
-config.cache_classes = false
+# The production environment is meant for finished, "live" apps.
+# Code is not reloaded between requests
+config.cache_classes = true
 
-# Log error messages when you accidentally call methods on nil.
-config.whiny_nils = true
+# Use a different logger for distributed setups
+# config.logger = SyslogLogger.new
 
-# Enable the breakpoint server that script/breakpointer connects to
-config.breakpoint_server = true
+# Full error reports are disabled and caching is turned on
+config.action_controller.consider_all_requests_local = false
+config.action_controller.perform_caching             = true
 
-# Show full error reports and disable caching
-config.action_controller.consider_all_requests_local = true
-config.action_controller.perform_caching             = false
-config.action_view.cache_template_extensions         = false
-config.action_view.debug_rjs                         = true
+# Enable serving of images, stylesheets, and javascripts from an asset server
+# config.action_controller.asset_host                  = "http://assets.example.com"
 
-# Don't care if the mailer can't send
-config.action_mailer.raise_delivery_errors = false
+# Disable delivery errors, bad email addresses will be ignored
+# config.action_mailer.raise_delivery_errors = false
+ActionMailer::Base.delivery_method = :sendmail
 
-# For console logging during development
-ActiveRecord::Base.logger = Logger.new(STDOUT)
+ActionMailer::Base.sendmail_settings = {
+  :location => "/usr/sbin/sendmail",
+  :arguments => "-t"
+}
+
+API_KEY = 'ABQIAAAAb5FIzoTRib2_GSmlbkQ7mBSGJ6blR4Z6uowhBF-VmNjSmr65kBSJuI180ZStGHLEeEzGK-YuA-gUTg'
