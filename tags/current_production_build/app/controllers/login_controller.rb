7fd87ee76c662dd3515adf0d2a5cfda73d24d550
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-10-07 21:33:21 +0000

    moving staging build to prod

---
+++ tags/current_production_build/app/controllers/login_controller.rb
@@ -0,0 +1,157 @@
+class LoginController < ApplicationController
+
+  # Be sure to include AuthenticationSystem in Application Controller instead
+  include AuthenticatedSystem
+  # If you want "remember me" functionality, add this before_filter to Application Controller
+  before_filter :login_from_cookie
+  
+  def login_small
+    index
+    if(!logged_in?)
+      render :action => "login/small", :layout => "login_small"
+    end
+  end
+  
+  def index
+
+    #Check if user is logged in and redirect to home controller if they are
+    if logged_in? 
+      user = self.current_user
+      if !user.remember_token_expires_at.nil? and Time.now < user.remember_token_expires_at 
+          redirect_back_or_default :controller => 'home' and return
+      else
+         cookies.delete :auth_token
+         self.current_user.forget_me
+         reset_session     
+      end    
+    end
+    
+    # Handles the login form post
+    if request.post?
+      # Authenticate based on un/pw as well as subdomain
+      if self.current_user = User.authenticate(request.subdomains.first, params[:email], params[:password])
+        if params[:remember_me] == "on"
+          self.current_user.remember_me
+          cookies['auth_token'] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
+          cookies['user_name'] = params[:email]
+        end
+        session[:user_id] = self.current_user.id # Store current user's id
+        session[:account_id] = self.current_user.account_id # Store the account id
+        session[:company] = self.current_user.account.company # Store the user's company name
+        session[:first_name] = self.current_user.first_name # Store user's first name
+        session[:email] = self.current_user.email # Store user's email
+        session[:is_super_admin] = self.current_user.is_super_admin
+        if params[:frm_m] == 'mobile'            
+          redirect_to(:controller=>'/mobile', :action=>'devices')  
+        else    
+          redirect_back_or_default(:controller => '/home', :action => 'index') # Login success
+        end
+      # Send them back to the login page with appropriate error message
+      else
+          flash[:message] = 'Please specify a valid username and password.'
+          flash[:username] = params[:email]
+          if params[:frm_m] == 'mobile'            
+             redirect_to(:controller=>'/mobile',:action=>'index')  
+          else    
+             redirect_back_or_default '/login'
+          end
+      end
+    # Display the appropriate login form based on subdomain
+    else
+      user = @session
+      @account = Account.find_by_subdomain(request.subdomains.first)
+      
+      # Subdomain does not exist so display appropriate form
+      if !@account
+        redirect_to :action => "sorry"        
+      end
+    end
+  end
+  
+ def forgot_password
+    flash[:message] = nil
+    if request.post?
+      account = Account.find_by_subdomain(request.subdomains.first)
+      user = User.find(:first, :conditions => ["email =? AND account_id =?", @params['email'], account.id])  
+      if user
+         key = user.generate_security_token
+         url = url_for(:action => 'password')
+         url += "?user[id]=#{user.id}&subdomain=#{user.account_id}&key=#{key}"
+         Notifier.deliver_forgot_password(user, url)
+         flash['message'] = "Plase check #{user.email} to change the password."
+         redirect_to :action => 'index'
+      else
+        flash[:message] = 'Please specify a valid username.'
+        render :action => 'forgot_password'
+      end  
+    end
+  end
+
+  def password
+    flash[:message] = nil
+    if request.post?
+        @user = User.find(params['id'])  
+        if @user
+          if(params['user']['password'] == params['user']['password_confirmation'])
+            @user.change_password(params['user']['password'], params['user']['password_confirmation'])
+            if @user.save
+              #Notifier.deliver_change_password(@user, params['user']['password'])
+              flash.now['notice'] = "New password is mailed to #{@user.email}"
+              redirect_to :action => 'index'     
+            else  
+               flash[:message] = 'Password change failed'
+               render :action => 'index'     
+            end
+          else
+            flash[:message] = "Passwords must match"
+            render :action => 'index'
+          end
+        else
+            flash[:message] = 'Please specify a valid username.'
+            render :action => 'index'     
+        end
+    end
+  end
+
+  def logout
+    self.current_user.forget_me if self.current_user!=:false
+    cookies.delete :auth_token
+    reset_session
+    flash[:notice] = "You have been logged out."
+    if params[:f_mb] == 'frm_mob'
+       redirect_to(:controller=>'mobile',:action=>'index')  
+    else    
+      redirect_back_or_default(:controller => '/login', :action => 'index')
+    end
+  end
+  
+
+   def login_from_cookie
+      return unless cookies['auth_token'] && @session[:user].nil?
+      user = User.find_by_remember_token(*cookies['auth_token'].split(","))     
+      if user && !user.remember_token_expires_at.nil? && Time.now < user.remember_token_expires_at 
+         session[:user] = user
+         session[:user_id] = user.id # Store current user's id
+         session[:account_id] = user.account_id # Store the account id
+         session[:company] = user.account.company # Store the user's company name
+         session[:first_name] = user.first_name # Store user's first name
+      end
+   end
+  
+   
+   def get_password_from_cookie
+      user = User.find_by_email(@params['id'])  
+      if user && !user.remember_token_expires_at.nil? && Time.now < user.remember_token_expires_at 
+         session[:user] = user
+         session[:user_id] = user.id # Store current user's id
+         session[:account_id] = user.account_id # Store the account id
+         session[:company] = user.account.company # Store the user's company name
+         session[:first_name] = user.first_name # Store user's first name 
+        @user = user
+        render :partial =>  'login/get_password_from_cookie', :user => @user, :layout => false
+     else
+        @email   = @params['id']
+        render :partial =>  'login/new_user', :email => @email, :layout => false
+     end   
+   end
+end
---
+++ tags/current_production_build/test/functional/login_controller_test.rb
@@ -0,0 +1,70 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'login_controller'
+
+# Re-raise errors caught by the controller.
+class LoginController; def rescue_action(e) raise e end; end
+
+class LoginControllerTest < Test::Unit::TestCase
+  
+  fixtures :users, :accounts
+  def setup
+    @controller = LoginController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+  end
+  
+  def test_index
+    get :index
+  end
+  
+  def test_login
+   @request.host="dennis.ublip.com"
+   post :index, {:email => users(:dennis).email, :password => "testing"} 
+   assert_redirected_to :controller => "home", :action => "index"
+   assert_equal accounts(:dennis).id, @request.session[:account_id]
+   assert_equal users(:dennis).id, @request.session[:user_id]
+   assert_equal users(:dennis).id, @request.session[:user]
+   assert_equal users(:dennis).email, @request.session[:email]
+   assert_equal users(:dennis).first_name, @request.session[:first_name]
+   assert_equal users(:dennis).account.company, @request.session[:company]
+ end
+ 
+ def test_login_same_email_diff_act
+   @request.host="nick.ublip.com"
+   post :index, {:email => users(:dennis2).email, :password => "testing"} 
+   assert_redirected_to :controller => "home", :action => "index"
+   assert_equal accounts(:nick).id, @request.session[:account_id]
+   assert_equal users(:dennis2).id, @request.session[:user_id]
+   assert_equal users(:dennis2).id, @request.session[:user]
+   assert_equal users(:dennis2).email, @request.session[:email]
+   assert_equal users(:dennis2).first_name, @request.session[:first_name]
+   assert_equal users(:dennis2).account.company, @request.session[:company]
+ end
+ 
+ def test_login_wrong_subdomain
+   @request.host="ken.ublip.com"
+   post :index, {:email => users(:dennis).email, :password => "testing"} 
+   assert_redirected_to "/login"
+   assert_nil @request.session[:account_id]
+   assert_nil @request.session[:user_id]
+ end
+ 
+ def test_login_failure
+   @request.host="dennis.ublip.com"
+   post :index, {:email => users(:dennis).email, :password => "wrong"} 
+   assert flash[:username]
+   assert_redirected_to "/login"
+ end
+ 
+ def test_logout
+   get :logout
+   assert_redirected_to "/login"
+ end
+ 
+ def test_change_password
+    post :password, {:id => "1", :user => {:password => "newpassword", :password_confirmation =>"newpassword"}}
+    user2 = User.find(1)
+    assert_equal User.encrypt("newpassword", "salty"), user2.crypted_password
+  end
+ 
+end
