139b17a46a477df205313eaa1781e2c8bd6384b2
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-10-23 21:22:05 +0000

    Changing gateway name to Xirgo

--- trunk/db/migrate/049_add_device_gateway_name.rb
+++ trunk/db/migrate/049_add_device_gateway_name.rb
@@ -2,7 +2,7 @@ class AddDeviceGatewayName < ActiveRecord::Migration
   def self.up
     add_column :devices,:gateway_name,:string
     
-    execute "update devices set gateway_name = 'enfora'"
+    execute "update devices set gateway_name = 'xirgo'"
   end
 
   def self.down
