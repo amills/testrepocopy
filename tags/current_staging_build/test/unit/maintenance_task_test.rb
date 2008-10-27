3c026f7217fdb84efef53ece0e7146c342e63c30
amills <amills@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2008-10-27 16:30:24 +0000

    moving build to staging

---
+++ tags/current_staging_build/test/unit/maintenance_task_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+require 'maintenance_task'
+
+class MaintenanceTaskTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
