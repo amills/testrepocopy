7cdaad3a1a6711ab646ea9957274c49679d3572e
bappelt <bappelt@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2009-01-06 18:41:44 +0000

    initial

--- db_procs.sql
+++ db_procs.sql
@@ -15,6 +15,8 @@ BEGIN
    * cos(((lng1 - lng2)*pi()/180))))*180/pi())*60*1.1515);
 END;;
 
+/*comment to make netbeans sql editor happy*/
+
 DROP PROCEDURE IF EXISTS insert_stop_event;;
 CREATE PROCEDURE insert_stop_event(
 	_latitude FLOAT,
@@ -229,6 +231,12 @@ BEGIN
 	END WHILE;
 END;;
 
+DROP PROCEDURE IF EXISTS create_stop_events;;
+CREATE PROCEDURE create_stop_events()
+BEGIN
+
+END;;
+
 DROP PROCEDURE IF EXISTS migrate_stop_data;;
 CREATE PROCEDURE migrate_stop_data()
 BEGIN
@@ -251,4 +259,13 @@ BEGIN
 		SELECT COUNT(*) INTO unprocessed_count FROM stops where processed=FALSE;
 	END;
 	END WHILE;
+END;;
+
+DROP TRIGGER IF EXISTS trig_readings_after_insert;;
+CREATE TRIGGER trig_readings_after_insert AFTER INSERT ON readings FOR EACH ROW BEGIN
+    DECLARE last_reading_time DATETIME;
+    SELECT r.created_at INTO last_reading_time FROM devices d,readings r WHERE d.id=NEW.device_id AND r.id=d.recent_reading_id;
+    IF NEW.created_at > last_reading_time OR last_reading_time IS NULL THEN
+        UPDATE DEVICES SET recent_reading_id=NEW.id WHERE id=NEW.device_id;
+    END IF;
 END;;
