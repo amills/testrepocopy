afde70f14973a3499a14c16c71f46c01c9f27867
semmons <semmons@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2009-01-20 03:50:53 +0000

    moving build to staging

---
+++ tags/current_staging_build/app/controllers/home_controller.rb
@@ -0,0 +1,23 @@
+class HomeController < ApplicationController
+  before_filter :authorize
+  
+  def index    
+    @device_count = Device.count(:all, :conditions => ['provision_status_id = 1 and account_id = ?', session[:account_id]])
+    @all_devices=Device.find(:all, :conditions=>['account_id=? and provision_status_id=1',session[:account_id]], :order=>'name', :include => :profile)                           
+    assign_the_selected_group_to_session 
+  end
+  
+  def show_devices                 
+    params[:group_type].nil? || params[:group_type]=="all" ?  session[:group_value]="all" : session[:group_value] = params[:group_type]
+    redirect_to :action=>'index'
+  end
+  
+  def map
+    render :action => "home/map", :layout => "map_only"   
+  end
+ 
+end
+
+
+
+
