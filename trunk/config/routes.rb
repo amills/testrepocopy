a2043094c7dc24d82470f47b1110ad3b2b3f2ebf
semmons <semmons@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2009-01-30 22:25:25 +0000

    globalize

--- trunk/config/routes.rb
+++ trunk/config/routes.rb
@@ -12,8 +12,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
-  map.connect '', :controller => "login"
-  #map.connect 'home', :controller => "devices" ,:action=>'show_group'
+  map.connect '', :controller => "home"
   map.connect 'logout', :controller => 'login', :action => 'logout'
   map.connect 'admin/accounts/:action', :controller => 'admin/accounts'
   map.connect 'admin/users/:action', :controller => 'admin/users'
@@ -36,5 +35,5 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id.:format'
   map.connect ':controller/:action/:id'
-  map.connect '*path', :controller=>'home'
+#  map.connect '*path', :controller=>'home'
 end
