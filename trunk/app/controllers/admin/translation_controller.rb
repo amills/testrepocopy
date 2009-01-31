fedcaedaa4aef21f33aeeeffc5b6d1fa16fc1ab9
semmons <semmons@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2009-01-31 18:50:19 +0000

    fix translation

--- trunk/app/controllers/admin/translation_controller.rb
+++ trunk/app/controllers/admin/translation_controller.rb
@@ -5,17 +5,22 @@ class Admin::TranslationController < ApplicationController
   layout 'admin'
   
   def index
+    @show_all = params[:show_all] == 'on'
     @language = Globalize::Language.find(:first,:conditions => {:iso_639_1 => (params[:language] || 'en')})
     if request.post? and params[:new_value] and params[:old_value]
       for key in params[:new_value].keys
         new_value = params[:new_value][key]
-        next if new_value.blank? or new_value == params[:old_value][key]
+        next if new_value == params[:old_value][key]
+        new_value = nil if new_value.blank?
         translation = Globalize::Translation.find(key)
         translation.text = new_value
         translation.save!
       end
     end
-    @translations = Globalize::Translation.find(:all,:conditions => {:language_id => @language.id}) # if @language.iso_639_1 != 'en'
+    return if @language.iso_639_1 == 'en'
+    conditions = "language_id = #{@language.id}"
+    conditions += " and text is null" unless @show_all
+    @translations = Globalize::Translation.find(:all,:conditions => conditions,:order => "type,tr_key")
   rescue
     flash[:error] = $!.to_s
   end
