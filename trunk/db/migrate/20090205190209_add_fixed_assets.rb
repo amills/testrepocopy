d8ca69beaf41e7d65b4a0057ef17d0a351a9f175
dbaldwin <dbaldwin@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2009-02-05 22:33:08 +0000

    Preliminary fixed asset implementation

---
+++ trunk/db/migrate/20090205190209_add_fixed_assets.rb
@@ -0,0 +1,15 @@
+class AddFixedAssets < ActiveRecord::Migration
+  def self.up
+    add_column :device_profiles, :is_fixed_asset, :boolean, :default => false 
+	  add_column :devices, :longitude, :decimal, :precision => 15, :scale => 10
+	  add_column :devices, :latitude, :decimal, :precision => 15, :scale => 10
+	  add_column :devices, :address, :string
+  end
+
+  def self.down
+    remove_column :device_profiles, :fixed_asset
+	  remove_column :devices, :latitude
+	  remove_column :devices, :longitude
+	  remove_column :devices, :address
+  end
+end
