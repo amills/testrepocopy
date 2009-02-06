56696c2766338d5d81bf940565f8b9d9e82348a4
semmons <semmons@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2009-02-06 18:06:18 +0000

    removing previous staging build

--- tags/current_staging_build/vendor/plugins/globalize_translator/uninstall.rb
+++
@@ -1,2 +0,0 @@
-FileUtils.rm_rf File.join(File.dirname(__FILE__), '../../../public/pt_window')  rescue nil
-FileUtils.rm_rf File.join(File.dirname(__FILE__), '../../../public/stylesheets/globalize_translator.css') rescue nil
