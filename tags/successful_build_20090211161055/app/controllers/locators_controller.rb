88dc203bc377cd172497a7d926e3966b2291c0f1
build <build@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2009-02-11 22:13:28 +0000

    successful build

---
+++ tags/successful_build_20090211161055/app/controllers/locators_controller.rb
@@ -0,0 +1,28 @@
+class LocatorsController < ApplicationController
+  before_filter :authorize
+  before_filter :authorize_device, :except => ['index']
+  
+  def index
+    @device_names = Device.get_names(session[:account_id])
+    redirect_to :action => 'edit',:id => @device_names[0].id if @device_names.any?
+  end
+
+  def edit
+    @device_names = Device.get_names(session[:account_id])
+    @device = Device.find(params[:id], :conditions => ["account_id = ?", session[:account_id]])
+    if @device.nil?
+      flash[:error] = 'Invalid action.'
+    elsif request.post?
+      if @device.update_attributes(params[:device])
+        enable_power_button = params[:power_key] == 'on'
+        @device.set_power_key(enable_power_button) if enable_power_button != @device.is_power_key_enabled?
+        flash[:success] = @device.name + ' was updated successfully'
+      else
+        flash[:error] =""
+        @device.errors.each_full do | err |
+          flash[:error] << err + "<br/>"
+        end
+      end
+    end
+  end
+end
