2b30f9a0937659c8717622e1388227b446b84b04
amills <amills@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2009-02-18 18:36:20 +0000

    Fixing case error.

--- db/migrate/20081114211002_breakout_address.rb
+++ db/migrate/20081114211002_breakout_address.rb
@@ -1,9 +1,9 @@
 class BreakoutAddress < ActiveRecord::Migration
   def self.up
-    execute "ALTER TABLE READINGS ADD COLUMN `geocoded` tinyint(1) NOT NULL default '0', ADD COLUMN `street_number` varchar(255), ADD COLUMN `street` varchar(255), ADD COLUMN `place_name` varchar(255), ADD COLUMN `admin_name1` varchar(255)"
+    execute "ALTER TABLE `readings` ADD COLUMN `geocoded` tinyint(1) NOT NULL default '0', ADD COLUMN `street_number` varchar(255), ADD COLUMN `street` varchar(255), ADD COLUMN `place_name` varchar(255), ADD COLUMN `admin_name1` varchar(255)"
   end
 
   def self.down
-    execute "ALTER TABLE READINGS DROP COLUMN `geocoded`, DROP COLUMN `street_number`, DROP COLUMN `street`, DROP COLUMN `place_name`, DROP COLUMN `admin_name1`"
+    execute "ALTER TABLE `readings` DROP COLUMN `geocoded`, DROP COLUMN `street_number`, DROP COLUMN `street`, DROP COLUMN `place_name`, DROP COLUMN `admin_name1`"
   end
 end
