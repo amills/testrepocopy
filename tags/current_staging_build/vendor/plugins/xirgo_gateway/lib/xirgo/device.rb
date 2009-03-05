4eeb6e28e465f70db437fd605be3fc5c798113ee
semmons <semmons@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2009-03-05 13:37:44 +0000

    moving build to staging

---
+++ tags/current_staging_build/vendor/plugins/xirgo_gateway/lib/xirgo/device.rb
@@ -0,0 +1,13 @@
+class Xirgo::Device < ActiveRecord::Base
+  attr_accessor :friendly_name
+  attr_accessor :logical_device
+  
+  def request_vin
+    Xirgo::CommandRequest.create! :device_id => self.id,:imei => self.imei,:command => '+XT:7002'
+  end
+  
+  def vin
+    @vin_readings ||= Xirgo::Reading.find(:all,:conditions => ['device_id = ? and vin is not null',self.id],:order => 'timestamp desc',:limit => 1)
+    @vin_readings[0].vin if @vin_readings.any?
+  end
+end
