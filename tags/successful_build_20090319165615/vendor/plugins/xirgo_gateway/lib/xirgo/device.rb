e98c2c29aa2442a43fa5490d6cbc65735025cf70
build <build@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2009-03-19 21:57:35 +0000

    successful build

---
+++ tags/successful_build_20090319165615/vendor/plugins/xirgo_gateway/lib/xirgo/device.rb
@@ -0,0 +1,21 @@
+class Xirgo::Device < ActiveRecord::Base
+  attr_accessor :friendly_name
+  attr_accessor :logical_device
+  
+  def request_vin
+    Xirgo::CommandRequest.create! :device_id => self.id,:imei => self.imei,:command => VIN_REQUEST,:start_date_time => Time.now
+  end
+  
+  def vin
+    @vin_readings ||= Xirgo::Reading.find(:all,:conditions => ['device_id = ? and vin is not null',self.id],:order => 'timestamp desc',:limit => 1)
+    @vin_readings[0].vin if @vin_readings.any?
+  end
+
+  def last_location_request
+    Xirgo::CommandRequest.find(:first,:conditions => {:device_id => self.id,:command => Xirgo::CommandRequest::LOCATION_REQUEST},:order => "start_date_time desc")
+  end
+  
+  def submit_location_request
+    Xirgo::CommandRequest.create!(:device_id => self.id,:imei => self.imei,:command => Xirgo::CommandRequest::LOCATION_REQUEST,:start_date_time => Time.now)
+  end
+end
