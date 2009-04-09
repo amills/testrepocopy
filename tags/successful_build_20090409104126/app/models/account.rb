ecfd3f18dae512812f344136dd4bee2873cfcb47
build <build@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2009-04-09 15:42:24 +0000

    successful build

---
+++ tags/successful_build_20090409104126/app/models/account.rb
@@ -0,0 +1,12 @@
+class Account < ActiveRecord::Base
+  belongs_to :dealer #an umbrella to aggregate accounts together
+  has_many :devices
+  has_many :users
+  has_many :groups
+  validates_uniqueness_of :subdomain, :case_sensitive => false, :message => "Please choose another subdomain; this one is already taken"
+  validates_presence_of :zip, :message => "Please specify your zip code"
+  validates_presence_of :company, :message => "Please specify your company or group name"
+  validates_presence_of :subdomain, :message => "Please specify a subdomain"
+  
+  # TODO eliminate the show_idle field with some future migration; it's not worth making a migration just for that...
+end
