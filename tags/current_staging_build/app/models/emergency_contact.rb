9989e10e2453c48105345dfad1f5b0ed31be8ee3
semmons <semmons@43da8e2a-8e9e-4132-97af-63a25ad0a55a>
2009-04-09 14:12:26 +0000

    removing previous staging build

--- tags/current_staging_build/app/models/emergency_contact.rb
+++
@@ -1,5 +0,0 @@
-class EmergencyContact < ActiveRecord::Base
-  belongs_to :driver
-  
-  validates_presence_of :name,:relationship
-end
--- tags/current_staging_build/test/unit/gateway_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-require 'gateway'
-
-class GatewayTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
-end
